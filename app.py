f5f4918298603e7c0a96cb7c236344fea43fbe16
Michael Thomas <mthoma5@tulane.edu>
2017-02-22 10:12:57 -0500

    Change port

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, url_for
 import requests
 import pandas as pd
 import re
+import os
 
 app = Flask(__name__)
 
@@ -82,6 +83,7 @@ def data():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
