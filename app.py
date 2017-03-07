443ac8db21693ddd0239e829205e49e2ef60ad05
ruby-choonoo <ruby.choonoo@gmail.com>
2017-03-07 13:47:38 -0500

    debug

--- app.py
+++ app.py
@@ -105,4 +105,5 @@ def data():
         pass
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port,debug= True)
+    debug = int(os.environ.get("DEBUG", False))
+    app.run(host='0.0.0.0', port=port,debug= debug)
