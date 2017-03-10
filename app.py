0077e8ad9351530556ad632114369e804151eb3e
ruby-choonoo <ruby.choonoo@gmail.com>
2017-03-09 22:49:12 -0500

    more style changes??

--- app.py
+++ app.py
@@ -111,5 +111,5 @@ def data():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     debug = int(os.environ.get("DEBUG", False))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
